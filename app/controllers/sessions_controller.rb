715d202a9c43bea7f7159c0eeacebf1f5c7c347f
Michael Wang <michael.icwang@gmail.com>
2015-09-20 11:31:11 +0800

    Implement user login.
    Using session with cookie which lost when user close browser.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    email = params[:session][:email]
+    password = params[:session][:password]
+    user = User.find_by(email: email.downcase)
+    if user && user.authenticate(password)
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password...'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   get 'help' => 'static_pages#help'
@@ -10,6 +12,12 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
 
   resources :users
+
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

793aebfce9639dc187fa686e6dbc0f5bf031964d
Michael Wang <michael.icwang@gmail.com>
2015-09-20 14:04:39 +0800

    Add more details to log in/out.
    * Change header based on login status.
    * Implement logout.
    * Auto login after sign up.
    * Add user in fixture for testing.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,22 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def current_user
+    if @current_user.nil?
+      @current_user = User.find_by(id: session[:user_id])
+    else
+      @current_user
+    end
+    # @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end

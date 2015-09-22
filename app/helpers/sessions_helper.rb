5979b8b93d348349d7c16f78202a97417b7fbae1
Michael Wang <michael.icwang@gmail.com>
2015-09-22 10:25:51 +0800

    Implement log out.
    * Also fix 2 logout related bugs (see 8.4.4).
    * Fix typo: cokkies -> cookies.
    * Save token to remember_digest (not remember_token).

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,7 +25,7 @@ module SessionsHelper
       # @current_user ||= User.find_by(id: session[:user_id])
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cokkies[:remember_token])
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -34,5 +34,19 @@ module SessionsHelper
 
   def logged_in?
     !current_user.nil?
+  end
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Logs out the current user.
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,10 +25,15 @@ class User < ActiveRecord::Base
 
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_token, User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end

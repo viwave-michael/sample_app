47748a17f5ad5706b2f559a97ffe6a5107363e79
Michael Wang <michael.icwang@gmail.com>
2015-10-01 10:39:08 +0800

    Add model for account activation and password reset.

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
 
   delete 'logout' => 'sessions#destroy'
 
+  resources :account_activations, only: [ :edit ]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
